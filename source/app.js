9950aa138f3b021398dd682924d0a39842c50b98
briptimus <brituor@gmail.com>
2016-01-23 13:50:18 -0800

    Creating your first stateless React component

--- source/app.js
+++ source/app.js
@@ -1,11 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item 1');
-var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item 2');
-var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item 3');
+var ReactClass = React.createClass({
+  render: function () {
+    if (this.props.isHidden) {
+      return null;
+    }
 
-var reactFragment = [ listItemElement1, listItemElement2, listItemElement3 ];
-var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
-
-ReactDOM.render(listOfItems, document.getElementById('react-application'));
+    var header = this.props.tweets.length + ' Latest Tweets';
+    return React.createElement('h1', { className: 'header' }, header);
+  }
+});
+var reactComponentElement = React.createElement(ReactClass);
+var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
