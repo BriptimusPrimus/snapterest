97932c424b424ff5f23a7e9d3264ca9a0a5a3c91
briptimus <brituor@gmail.com>
2016-01-25 21:17:41 -0800

    Creating a Collection component

--- source/app.jsx
+++ source/app.jsx
@@ -2,4 +2,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Application = require('./components/Application.react.jsx');
 
-ReactDOM.render(Application, document.getElementById('react-application'));
+ReactDOM.render(<Application />, document.getElementById('react-application'));
